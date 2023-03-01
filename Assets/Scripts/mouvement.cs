0ca03963d381f1cec66064c2aaddfa5223766c20
dieuxgragon <enzonussbaum33@gmail.com>
2023-03-01 13:48:24 +0100

    deplacement perso

---
+++ Assets/Scripts/mouvement.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+public class mouvement : MonoBehaviour
+{
+    [SerializeField]
+    private int maxMouvement;
+    [SerializeField]
+    private int Mouvement;
+    [SerializeField]
+    private int minMouvement;
+    [SerializeField]
+    private bool canmoove;
+    private void Start()
+    {
+        
+    }
+    void Update()
+    {
+        if (canmoove){
+            canmoove = false;
+            if (Input.GetKeyDown(KeyCode.UpArrow) && Mouvement <= maxMouvement)
+            {
+                Debug.Log("test");
+                transform.Translate(0, 2.5f, 0);
+                Mouvement++;
+            }
+            else if (Input.GetKeyDown(KeyCode.DownArrow) && Mouvement >= -minMouvement)
+            {
+                transform.Translate(0, -2.5f, 0);
+                Mouvement--;
+            }
+            canmoove =true;
+        }
+    }
+}
