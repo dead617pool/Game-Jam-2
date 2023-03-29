9f966239c47dcfa3a38231c147c5768dbbbcb710
WaliWalou <118349522+WaliWalou@users.noreply.github.com>
2023-03-29 10:26:17 +0200

    Scrolling level - Speed + Parallax + UI Controller (#4)
    * Speed player + UI score (distance)
    
    * Update Distance
    
    * Parallax Background
    
    * Update Speed.cs
    
    * Update Scene_jayson.unity
    
    * Ajout HighScore + sauvegarde du HighScore
    
    * Affichage HighScore + ajouts fonts
    
    * Delete copy scene jayson 1

---
+++ Assets/Scripts/Speed.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Speed : MonoBehaviour
+{
+
+    public Vector2 velocity;
+    public float maxXVelocity = 300;
+    public float maxAcceleration = 7;
+    public float acceleration = 7;
+    public float distance = 0;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate()
+    {
+
+        distance += (velocity.x / 5) * Time.fixedDeltaTime;
+
+        Vector2 pos = transform.position;
+
+        float velocityRatio = velocity.x / maxXVelocity;
+        acceleration = maxAcceleration * (1 - velocityRatio);
+
+        velocity.x += acceleration * Time.fixedDeltaTime;
+        if (velocity.x >= maxXVelocity)
+        {
+            velocity.x = maxXVelocity;
+        }
+
+        transform.position = pos;
+    }
+
+    
+
+}
---
+++ Assets/Scripts/UIController.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+
+    Speed speed;
+    Text distanceText;
+    public Text highScoreText;
+
+    private void Awake()
+    {
+        speed = GameObject.Find("Player").GetComponent<Speed>();
+        distanceText = GameObject.Find("DistanceText").GetComponent<Text>();
+        highScoreText = GameObject.Find("HighScoreText").GetComponent<Text>();
+    }
+
+    void Start()
+    {
+        highScoreText.text = PlayerPrefs.GetInt("HighScoreText", 0).ToString();
+    }
+
+
+    void Update()
+    {
+        int distance = Mathf.FloorToInt(speed.distance);
+        distanceText.text = "Score : " + distance;
+
+        if (distance > PlayerPrefs.GetInt("HighScoreText", 0))
+        {
+            PlayerPrefs.SetInt("HighScoreText", distance);
+            highScoreText.text = distance.ToString();
+        }
+        
+    }
+
+}
