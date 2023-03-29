2aef993aafeda057ce207116fa2c51c48aa1dd2a
dead617pool <florian.pradier@ynov.com>
2023-03-29 15:35:36 +0200

    add abstract for futur Bonus implantations

---
+++ Assets/Scripts/Bonus.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public abstract class Bonus : MonoBehaviour
+    {
+        [SerializeField] private Rigidbody2D rigidbody;
+        [SerializeField] private SpriteRenderer sprite;
+        [SerializeField] private BoxCollider2D hitbox;
+        [SerializeField] private int pointsNeededToUnlock;
+        [SerializeField] private string name { get; }
+
+        public bool IsUnlocked(int hitscore) { return hitscore >= pointsNeededToUnlock; }
+
+        public abstract void Effect(Player player);
+
+    }
+}
